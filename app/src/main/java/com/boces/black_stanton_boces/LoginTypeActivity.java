48932f2116e12c92939b3370cd1d229bf7edc3d7
TheUnstableLiz <e.c.stanton@eagle.clarion.edu>
2017-10-17 19:24:10 -0400

    first commit

---
+++ app/src/main/java/com/boces/black_stanton_boces/LoginTypeActivity.java
@@ -0,0 +1,13 @@
+package com.boces.black_stanton_boces;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginTypeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_type);
+    }
+}

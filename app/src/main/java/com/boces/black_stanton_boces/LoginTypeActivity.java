be48d5597efb38f035d740f527d0e5ecb17763a4
Liz <e.c.stanton@eagle.clarion.edu>
2017-11-01 20:50:03 -0400

    errors keeping prroject from building fixed

--- app/src/main/java/com/boces/black_stanton_boces/LoginTypeActivity.java
+++ app/src/main/java/com/boces/black_stanton_boces/LoginTypeActivity.java
@@ -1,7 +1,9 @@
 package com.boces.black_stanton_boces;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginTypeActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class LoginTypeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_type);
+
+    }
+    //Open Student Login Process (Teacher Select)
+    public void onClickStudentLogin(View v)
+    {
+        startActivity(new Intent( this, StudentLoginSelectTeacherActivity.class));
+    }
+    //Open Admin Login Process (Admin Login)
+    private void onClickAdminLogin(View v)
+    {
+        startActivity(new Intent(this, AdminLoginActivity.class));
     }
 }

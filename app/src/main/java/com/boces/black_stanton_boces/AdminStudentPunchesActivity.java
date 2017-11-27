852059ca02a697e3c566fd6ad1927dc0189b735e
Liz <e.c.stanton@eagle.clarion.edu>
2017-11-27 10:30:46 -0500

    Punches page and time item created and designed(not tested)

---
+++ app/src/main/java/com/boces/black_stanton_boces/AdminStudentPunchesActivity.java
@@ -0,0 +1,14 @@
+package com.boces.black_stanton_boces;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AdminStudentPunchesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_student_punches);
+    }
+}

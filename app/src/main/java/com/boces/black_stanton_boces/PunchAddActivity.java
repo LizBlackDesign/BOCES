51e2eca08808d0f651a4e0702f5f37493790b58a
TheUnstableLiz <e.c.stanton@eagle.clarion.edu>
2017-11-26 21:04:06 -0500

    pages dor reports added

---
+++ app/src/main/java/com/boces/black_stanton_boces/PunchAddActivity.java
@@ -0,0 +1,13 @@
+package com.boces.black_stanton_boces;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PunchAddActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_punch_add);
+    }
+}

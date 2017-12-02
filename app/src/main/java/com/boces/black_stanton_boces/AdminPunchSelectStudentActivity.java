5ff821798fe7387f91e50eb9acc660abe4bfd518
TheUnstableLiz <e.c.stanton@eagle.clarion.edu>
2017-12-02 14:44:30 -0500

    admin puch select student activity created and designed (no java)

---
+++ app/src/main/java/com/boces/black_stanton_boces/AdminPunchSelectStudentActivity.java
@@ -0,0 +1,13 @@
+package com.boces.black_stanton_boces;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AdminPunchSelectStudentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_punch_select_student);
+    }
+}

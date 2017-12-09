94af3c24f5fcf72314f68c5cc8907e57a5276ab3
Evan Black <me@evanpblack.com>
2017-12-09 17:46:00 -0500

    Add File Picker to Tasks #20

--- app/src/main/java/com/boces/black_stanton_boces/AdminEditTaskActivity.java
+++ app/src/main/java/com/boces/black_stanton_boces/AdminEditTaskActivity.java
@@ -1,13 +1,18 @@
 package com.boces.black_stanton_boces;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -29,6 +34,9 @@ public class AdminEditTaskActivity extends AppCompatActivity {
     public enum BUNDLE_KEY {
         TASK_ID
     }
+
+    private static final int EXTERNAL_STORAGE_REQUEST = 0;
+    private static final int RESULT_LOAD_IMAGE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -75,30 +83,52 @@ public class AdminEditTaskActivity extends AppCompatActivity {
     }
 
     public void onCamera(View v) {
-        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(cameraIntent, 1);
-        cameraIntent.setType("image/*");
-        cameraIntent.putExtra("crop", "true");
-        cameraIntent.putExtra("aspectX", 0);
-        cameraIntent.putExtra("aspectY", 0);
-        cameraIntent.putExtra("outputX", 250);
-        cameraIntent.putExtra("outputY", 200);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, EXTERNAL_STORAGE_REQUEST);
+            return;
+        }
+        Intent mediaIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(mediaIntent, RESULT_LOAD_IMAGE);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        Bundle extras = data.getExtras();
+        super.onActivityResult(requestCode, resultCode, data);
 
-        if (extras == null) {
-            Toast.makeText(this, "No Image Passed Back", Toast.LENGTH_LONG).show();
-            return;
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            Uri selectedImage = data.getData();
+            String filePathColumn[] = { MediaStore.Images.Media.DATA };
+            Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+            String path = "";
+
+            if (cursor == null)
+                return;
+
+            if (cursor.moveToFirst()) {
+                path = cursor.getString(cursor.getColumnIndex(filePathColumn[0]));
+            }
+            cursor.close();
+
+            if (path.isEmpty())
+                return;
+
+            image = BitmapFactory.decodeFile(path);
+            if (image == null)
+                return;
+
+            imageView.setImageBitmap(image);
         }
-        image = extras.getParcelable("data");
-        if (image == null) {
-            Toast.makeText(this, "No Image Passed Back", Toast.LENGTH_LONG).show();
-            return;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case EXTERNAL_STORAGE_REQUEST:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    onCamera(null);
+                }
+                break;
         }
-        imageView.setImageBitmap(image);
     }
 
     public void onDelete(View view) {

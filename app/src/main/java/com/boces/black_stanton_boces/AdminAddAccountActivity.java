d12fd57a365b1262ad1de69b425e2946abe1df08
Evan Black <me@evanpblack.com>
2017-12-11 21:02:50 -0500

    Validate Accounts #22

--- app/src/main/java/com/boces/black_stanton_boces/AdminAddAccountActivity.java
+++ app/src/main/java/com/boces/black_stanton_boces/AdminAddAccountActivity.java
@@ -43,17 +43,34 @@ public class AdminAddAccountActivity extends AppCompatActivity {
      */
     public void onSave(View v) {
         PersistenceInteractor persistence = new PersistenceInteractor(this);
+        boolean hasError = false;
 
-        if (username.getText().toString().isEmpty())
-            return;
-        if (password.getText().toString().isEmpty())
-            return;
-        if (confirmPassword.getText().toString().isEmpty())
-            return;
+        if (username.getText().toString().isEmpty()) {
+            username.setError("Username Is Required");
+            hasError = true;
+        }
+        if (password.getText().toString().isEmpty()) {
+            hasError = true;
+            password.setError("Password Is Required");
+        }
+
+        if (confirmPassword.getText().toString().isEmpty()) {
+            hasError = true;
+            confirmPassword.setError("Password Must Be Confirmed");
+        }
 
         //Checks both passwords to assure they match
-        if (password.getText().toString().equals(confirmPassword.getText().toString()))
-            persistence.createAdminAccount(username.getText().toString(), password.getText().toString());
+        // Also Make Sure One Isn't Empty
+        if (!password.getText().toString().equals(confirmPassword.getText().toString()) && !password.getText().toString().isEmpty()) {
+            hasError = true;
+            confirmPassword.setError("Passwords Must Match");
+        }
+
+        // Stop Runs With Errors
+        if (hasError)
+            return;
+
+        persistence.createAdminAccount(username.getText().toString(), password.getText().toString());
 
         finish(); //Ends activity
     }
